24f0c44cc7a7e8b7485ad04300408e5eb7c4d051
jamie <jamiecheng9@gmail.com>
2017-08-03 23:51:04 +0200

    dot benchmaerk

--- benchmark/src/benchmark.cpp
+++ benchmark/src/benchmark.cpp
@@ -12,41 +12,57 @@ using array = nd::array<double>;
 //    ndarray benchmark    //
 /////////////////////////////
 
-static void BM_transpose(benchmark::State& state) {
+static void BM_transpose(benchmark::State &state) {
     array a({100, 100}, 0.0);
 
     while (state.KeepRunning()) {
         a.transpose();
     }
 }
+
 BENCHMARK(BM_transpose);
 
-static void BM_at(benchmark::State& state) {
+static void BM_at(benchmark::State &state) {
     array a({100, 100}, 0.0);
 
     while (state.KeepRunning()) {
         a.at(10);
     }
 }
+
 BENCHMARK(BM_at);
 
-static void BM_item(benchmark::State& state) {
+static void BM_item(benchmark::State &state) {
     array a({100, 100}, 0.0);
 
     while (state.KeepRunning()) {
         a.item({10, 10});
     }
 }
+
 BENCHMARK(BM_item);
 
-static void BM_sum(benchmark::State& state) {
+static void BM_sum(benchmark::State &state) {
     array a({100, 100}, 0.0);
 
     while (state.KeepRunning()) {
         a + 1.0;
     }
 }
+
 BENCHMARK(BM_sum);
+
+static void BM_dot(benchmark::State &state) {
+    array a({500, 500}, 0.0);
+
+    array b({500, 500}, 0.0);
+
+    while (state.KeepRunning()) {
+        a.dot(b);
+    }
+}
+
+BENCHMARK(BM_dot);
 
 /////////////////////////////
 //    old lib benchmark    //
@@ -59,6 +75,7 @@ static void BM_nnet_sum(benchmark::State &state) {
         a + 1;
     }
 }
+
 BENCHMARK(BM_nnet_sum);
 
 static void BM_nnet_transpose(benchmark::State &state) {
@@ -68,6 +85,7 @@ static void BM_nnet_transpose(benchmark::State &state) {
         a.transponse();
     }
 }
+
 BENCHMARK(BM_nnet_transpose);
 
 static void BM_nnet_item(benchmark::State &state) {
@@ -77,6 +95,19 @@ static void BM_nnet_item(benchmark::State &state) {
         a[10][10];
     }
 }
+
 BENCHMARK(BM_nnet_item);
+
+static void BM_nnet_dot(benchmark::State &state) {
+    nnet::Array a(500, 500);
+
+    nnet::Array b(500, 500);
+
+    while (state.KeepRunning()) {
+        a.dot(b);
+    }
+}
+
+BENCHMARK(BM_nnet_dot)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
